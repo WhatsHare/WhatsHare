03135cfd3bb38dc0eb271dca7721add2169eda87
mb <michele.bonazza@gmail.com>
2013-06-22 18:55:16 +0200

    Added license information to all files and to the about dialog

--- Whatshare/src/it/mb/whatshare/SendToAppActivity.java
+++ Whatshare/src/it/mb/whatshare/SendToAppActivity.java
@@ -1,3 +1,21 @@
+/**
+ * SendToAppActivity.java Created on 16 Jun 2013 Copyright 2013 Michele Bonazza
+ * <emmepuntobi@gmail.com>
+ * 
+ * Copyright 2013 Michele Bonazza <emmepuntobi@gmail.com> This file is part of WhatsHare.
+ * 
+ * WhatsHare is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Foobar is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * WhatsHare. If not, see <http://www.gnu.org/licenses/>.
+ */
 package it.mb.whatshare;
 
 import android.app.Activity;
